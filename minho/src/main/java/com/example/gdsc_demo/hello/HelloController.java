48dce05b98380d11ab1b1e2de2df989ed9dea103
Min Ho CHO <chominho96@naver.com>
2022-10-31 21:32:25 +0900

    Add Spring Project

---
+++ minho/src/main/java/com/example/gdsc_demo/hello/HelloController.java
@@ -0,0 +1,13 @@
+package com.example.gdsc_demo.hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello World!";
+    }
+}
